6f616eca14e9192283cc7260f7b9b60422b7e3c8
matuxer <matias@biott.com.ar>
2025-10-21 16:56:59 -0300

    Agregado el Repository, DAO y Controller de la clase Product

---
+++ src/main/java/com/example/ferreteria/repository/ProductoRepository.java
@@ -0,0 +1,11 @@
+package com.example.ferreteria.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.ferreteria.model.ProductoModel;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<ProductoModel, Long> {
+
+}
