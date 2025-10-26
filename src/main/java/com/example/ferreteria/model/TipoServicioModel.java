48eb47a9c65209ef94a3bb947912f69eaa4f475a
matuxer <matias@biott.com.ar>
2025-10-26 20:31:35 -0300

    Fix naming conventions and update table name in TipoServicioModel

--- src/main/java/com/example/ferreteria/model/TipoServicioModel.java
+++ src/main/java/com/example/ferreteria/model/TipoServicioModel.java
@@ -1,26 +1,29 @@
 package com.example.ferreteria.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tipos servicios")
+@Table(name = "tipos_servicios")
 public class TipoServicioModel {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nombre", length = 50 )
-    private String Nombre;
+    @Column(name = "nombre", length = 50)
+    private String nombre;
 
     public TipoServicioModel() {
     }
 
     public TipoServicioModel(Long id, String nombre) {
         this.id = id;
-        Nombre = nombre;
+        this.nombre = nombre;
     }
 
     public Long getId() {
@@ -32,10 +35,10 @@ public class TipoServicioModel {
     }
 
     public String getNombre() {
-        return Nombre;
+        return nombre;
     }
 
     public void setNombre(String nombre) {
-        Nombre = nombre;
+        this.nombre = nombre;
     }
 }
