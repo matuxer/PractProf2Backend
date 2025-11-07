9ba4a27e35a489c0690a0987b2f5cb33feae1e4c
matuxer <matias@biott.com.ar>
2025-11-06 22:31:56 -0300

    Reorganizar importaciones en ProductoCategoriaModel para mejorar la legibilidad

--- src/main/java/com/example/ferreteria/model/ProductoCategoriaModel.java
+++ src/main/java/com/example/ferreteria/model/ProductoCategoriaModel.java
@@ -1,14 +1,17 @@
 package com.example.ferreteria.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import java.util.List;
-import java.util.ArrayList;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "producto_categorias")
@@ -23,6 +26,7 @@ public class ProductoCategoriaModel {
 
     // Relación con la tabla productos
     @OneToMany(mappedBy = "categoria")
+    @JsonManagedReference
     private List<ProductoModel> productos = new ArrayList<>();
 
     //Getters y Setters
